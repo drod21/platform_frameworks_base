12b175fc8ece854aaead7216efca7e8d7a2d76db
Derek Rodriguez <drod2169@gmail.com>
2012-04-02 15:59:08 -0400

    Enable -flto flag as well
    Change-Id: Ia4f7dccba381fc433a2ba467558039b0d6467d3a

--- opengl/libs/Android.mk
+++ opengl/libs/Android.mk
@@ -18,9 +18,9 @@ LOCAL_SRC_FILES:= 	       \
 	EGL/Loader.cpp 	       \
 #
 
-LOCAL_CFLAGS += -ffast-math
+LOCAL_CFLAGS += -flto -ffast-math
 LOCAL_SHARED_LIBRARIES += libcutils libutils libGLESv2_dbg
-LOCAL_LDLIBS := -lpthread -ldl
+LOCAL_LDLIBS := -flto -lpthread -ldl
 LOCAL_MODULE:= libEGL
 LOCAL_LDFLAGS += -Wl,--exclude-libs=ALL
 LOCAL_SHARED_LIBRARIES += libdl
@@ -83,8 +83,8 @@ LOCAL_SRC_FILES:= 		\
 #
 
 LOCAL_SHARED_LIBRARIES += libcutils libEGL
-LOCAL_CFLAGS += -ffast-math
-LOCAL_LDLIBS := -lpthread -ldl
+LOCAL_CFLAGS += -flto -ffast-math
+LOCAL_LDLIBS := -flto -lpthread -ldl
 LOCAL_MODULE:= libGLESv1_CM
 
 LOCAL_SHARED_LIBRARIES += libdl
@@ -115,9 +115,9 @@ LOCAL_SRC_FILES:= 		\
 	GLES2/gl2.cpp.arm 	\
 #
 
-LOCAL_CFLAGS += -ffast-math
+LOCAL_CFLAGS += -flto -ffast-math
 LOCAL_SHARED_LIBRARIES += libcutils libEGL
-LOCAL_LDLIBS := -lpthread -ldl
+LOCAL_LDLIBS := -flto -lpthread -ldl
 LOCAL_MODULE:= libGLESv2
 
 LOCAL_SHARED_LIBRARIES += libdl
@@ -162,8 +162,8 @@ LOCAL_SRC_FILES:= 		\
 	ETC1/etc1.cpp 	\
 #
 
-LOCAL_CFLAGS += -ffast-math
-LOCAL_LDLIBS := -lpthread -ldl
+LOCAL_CFLAGS += -flto -ffast-math
+LOCAL_LDLIBS := -flto -lpthread -ldl
 LOCAL_MODULE:= libETC1
 
 include $(BUILD_SHARED_LIBRARY)
