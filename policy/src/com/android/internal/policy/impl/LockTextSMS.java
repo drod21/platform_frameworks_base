acde5d537c3bb200c41e3d1e51c7f1c027c3c60a
Derek Rodriguez <drod2169@gmail.com>
2012-05-09 14:32:14 -0400

    Fix bootloop stuffs and music player stuffs

--- policy/src/com/android/internal/policy/impl/LockTextSMS.java
+++ policy/src/com/android/internal/policy/impl/LockTextSMS.java
@@ -50,6 +50,7 @@ public class LockTextSMS extends TextView {
     private Handler mHandler;
     
     private static final String SMS_RECEIVED = "android.provider.Telephony.SMS_RECEIVED";
+    private static final String ACTION_SHUTDOWN = "android.intent.action.ACTION_SHUTDOWN";
     private static final String TAG = "LockTextSMS";
     
     private String body;
@@ -82,10 +83,15 @@ public class LockTextSMS extends TextView {
 
         if (!mIsAttached) {
             mIsAttached = true;
+
             IntentFilter filter = new IntentFilter();
 
+            IntentFilter filter2 = new IntentFilter();
+
             filter.addAction(SMS_RECEIVED);
+            filter2.addAction(ACTION_SHUTDOWN);
             getContext().registerReceiver(mIntentReceiver, filter, null, getHandler());
+            getContext().registerReceiver(mShutDownReceiver, filter2, null, getHandler());
         }
     }
 
@@ -98,6 +104,7 @@ public class LockTextSMS extends TextView {
         if (showTexts) {
         	if (mIsAttached) {
                 getContext().unregisterReceiver(mIntentReceiver);
+                getContext().unregisterReceiver(mShutDownReceiver);
                 mIsAttached = false;
             }
         }
@@ -116,41 +123,55 @@ public class LockTextSMS extends TextView {
             }
         }
     };
-    
+
+    private final BroadcastReceiver mShutDownReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.i(TAG, "Intent recieved: " + intent.getAction());
+            //needed to search for shutdown to prevent hotboots upon reboots with 
+            //SMS popup visible
+            if (intent.getAction() == ACTION_SHUTDOWN) {
+              Settings.System.putInt(getContext().getContentResolver(), Settings.System.LOCKSCREEN_SMS_CROSS, 1);
+            }
+        }
+    };
+        
     private void keepMyBoxUp() {
     	boolean showTexts = (Settings.System.getInt(mContext.getContentResolver(), Settings.System.LOCKSCREEN_SMS_CROSS, 1) == 0);
 	boolean musicPlaying = (Settings.System.getInt(mContext.getContentResolver(), Settings.System.LOCKSCREEN_SMS_MUSIC, 0) == 1);
     	
-    	Uri uri = Uri.parse("content://sms/inbox");
+	if (showTexts) {
+    	  Uri uri = Uri.parse("content://sms/inbox")
 
-    	Cursor c = mContext.getContentResolver().query(uri, null, "read = 0", null, null);
-    	int unreadSMSCount = c.getCount();
-    	c.deactivate();
-    	
-    	if (unreadSMSCount > 0 && showTexts && !musicPlaying) {
-    		String name = null;
-        	String msg = null;
-        	Cursor cursor1 = mContext.getContentResolver().query(uri,new String[] { "_id", "thread_id", "address", "person", "date","body", "type" }, null, null, null);
-        	String[] columns = new String[] { "address", "body"};
-        	if (cursor1.getCount() > 0) {
-        	   if (cursor1.moveToFirst()){
-        	       name = cursor1.getString(cursor1.getColumnIndex(columns[0]));
-        	       msg = cursor1.getString(cursor1.getColumnIndex(columns[1]));
-        	    }
-        	}
-        	if (msg != null && name != null) {
-            	Uri personUri = Uri.withAppendedPath( ContactsContract.PhoneLookup.CONTENT_FILTER_URI, name);
-            	Cursor cur = mContext.getContentResolver().query(personUri, new String[] { ContactsContract.PhoneLookup.DISPLAY_NAME }, null, null, null );
-            	if (cur.moveToFirst()) {
-            		int nameIndex = cur.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME);
-            		caller = cur.getString(nameIndex);
+          Cursor c = mContext.getContentResolver().query(uri, null, "read = 0", null, null);
+          int unreadSMSCount = c.getCount();
+          c.deactivate();
+          
+          if (unreadSMSCount > 0 && !musicPlaying) {
+            String name = null;
+              String msg = null;
+              Cursor cursor1 = mContext.getContentResolver().query(uri,new String[] { "_id", "thread_id", "address", "person", "date","body", "type" }, null, null, null);
+              String[] columns = new String[] { "address", "body"};
+              if (cursor1.getCount() > 0) {
+                 if (cursor1.moveToFirst()){
+                     name = cursor1.getString(cursor1.getColumnIndex(columns[0]));
+                     msg = cursor1.getString(cursor1.getColumnIndex(columns[1]));
+                  }
             	}
-            	if (caller == null) {
-            		caller = name;
+              if (msg != null && name != null) {
+                  Uri personUri = Uri.withAppendedPath( ContactsContract.PhoneLookup.CONTENT_FILTER_URI, name);
+                  Cursor cur = mContext.getContentResolver().query(personUri, new String[] { ContactsContract.PhoneLookup.DISPLAY_NAME }, null, null, null );
+                  if (cur.moveToFirst()) {
+                    int nameIndex = cur.getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME);
+                    caller = cur.getString(nameIndex);
+                  }
+                  if (caller == null) {
+                    caller = name;
+                  }
+                  body = msg;
+                  updateCurrentText(body, caller);
+                  setBackgroundResource(R.drawable.ic_lockscreen_player_background_old);
             	}
-            	body = msg;
-            	updateCurrentText(body, caller);
-            	setBackgroundResource(R.drawable.ic_lockscreen_player_background_old);
         	}
     	}
     }
